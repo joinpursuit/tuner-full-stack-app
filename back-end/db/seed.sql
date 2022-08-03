cbda45c283ff0bf9bf41b5bfdceec00a1fd38897
Spencer Simon <spencer.san@gmail.com>
2022-08-03 19:12:15 -0400

    songs added to seed.sql

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,6 +1,10 @@
-\c bookmarks_dev
+\c songs_dev
 
-INSERT INTO bookmarks (name, url, category, is_favorite) VALUES
-('MDN', 'https://developer.mozilla.org/en-US/', 'educational', true),
-('Apartment Therapy', 'https://www.apartmenttherapy.com', 'inspirational', true),
-('DMV', 'https://dmv.ny.gov', 'adulting', true);
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Life Goes On', '2Pac', 'All Eyez On Me', '5:01', TRUE),
+('Stan', 'Eminem', 'The Marshall Mathers LP', '6:44', TRUE),
+('Down and Out', "Cam'ron", 'Purple Haze', '3:37', TRUE),
+('Beggin', 'Madcon', 'Conquest', '4:48', FALSE),
+('Renee', 'Lost Boyz', 'Legal Drug Money', '4:54', TRUE),
+('m.A.A.d city', 'Kendrick Lamar', 'good kid, m.A.A.d city', '5:50', TRUE),
+("What Up Gangsta", '50 Cent', "Get Rich or Die Tryin", '2:59', TRUE);
