98f9fd114f2f5f11895c52304807708c8b01f53a
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-08-02 21:54:14 -0400

    inital edits

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from './Pages/Home.js';
+import FourOFour from "./Pages/FourOFour.js";
+
 function App() {
   return (
-    <div className="">
-      <h1>Tuner App</h1>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Drafted Routes Below!!! */}
+          {/* <Route path="/songs" element={<Index />} />
+          <Route path="/songs/new" element={<New />} />
+          <Route path="/songs/:index" element={<Show />} />
+          <Route path="/songs/:index/edit" element={<Edit />} /> */}
+
+          <Route path="*" element={<FourOFour />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ front-end/src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="Tuner-App">
+            <h1>Tuner App</h1>
+        </div>
+    );
+}
+
+export default Home;
