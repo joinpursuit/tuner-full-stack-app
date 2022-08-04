81caddbdbfe1a9d5d5018389f048259f5f4f03fd
ziqian pan <ziqianpan1998@gmail.com>
2022-08-03 22:13:06 -0400

    got part 2 finished with show/post with validation

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js")
+const { getAllSongs, getSong,createSong } = require("../queries/songs.js");
+const {checkName, checkBoolean} = require("../validations/checkSongs.js");
 
 songs.get("/", async (req, res) => {
 
     const allSongs = await getAllSongs();
-    
+
     if (allSongs[0]) {
         res.status(200).json(allSongs);
     } else {
         res.status(500).json({ error: "server error!"})
     }
 });
+
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if (song) {
+        res.json(song)
+    } else {
+        res.status(404).json({error: "not found"});
+    }
+})
+
+songs.post("/", checkName, checkBoolean, async (req, res) => {
+    try {
+        const song = await createSong(req.body);
+        res.json(song);
+    } catch (error) {
+        return error;
+    }
+})
+
 
 module.exports = songs;
