ada2590b31cc6ed6e151daa1c2803997cd1ee0b7
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-04 17:04:02 -0400

    server

---
+++ back-end/app.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Bookmarks App");
+});
+
+// EXPORT
+module.exports = app;
