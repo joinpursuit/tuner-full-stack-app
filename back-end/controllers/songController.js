01264f902393cb36da29e95ebc7f2e4860d1acb0
Matt M <Afrodesiacc@gmail.com>
2022-08-05 03:41:32 -0400

    Minor adjustments.

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -6,16 +6,20 @@ const db = require("../db/dbConfig");
 // Index of all listed Songs
 songs.get("/", async (req, res) => {
   const allSongs = await db.any("SELECT * FROM song");
-  console.log(allSongs)
+  console.log(allSongs);
   res.json({ success: true, payload: allSongs });
 });
 
 //Individual Song
 songs.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const songs = await db.one("SELECT * FROM song WHERE id=$1", id)
+  const songs = await db.one("SELECT * FROM song WHERE id=$1", id);
   console.log(id);
-  res.status(200).json({ success: true, payload: songs });
+  if (songs) {
+    res.status(200).json({ success: true, payload: songs });
+  } else {
+    res.status(404).send(`No track exists with the id of ${id}`);
+  }
 });
 
 // Create a new song
@@ -23,7 +27,16 @@ songs.post("/new", (req, res) => {
   const newSong = req.body;
   console.log(newSong);
   // favSongs.push(newSong);
-  const newSongs = db.any("INSERT INTO song (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *", [req.body.name, req.body.artist, req.body.album, req.body.time, req.body.is_favorite])
+  const newSongs = db.any(
+    "INSERT INTO song (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [
+      req.body.name,
+      req.body.artist,
+      req.body.album,
+      req.body.time,
+      req.body.is_favorite,
+    ]
+  );
   res.status(200).json({ success: true, payload: newSongs });
 });
 
