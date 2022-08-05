12cf6bce81a452ed8d98ff784e40a2b22e9d2c1e
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-05 14:22:07 -0400

    create

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs,getSong } = require("../queries/songs");
+const { getAllSongs,getSong, createSong } = require("../queries/songs");
 
 // INDEX
   songs.get("/", async (req, res) => {
@@ -23,6 +23,15 @@ songs.get("/:id", async (req, res) => {
     }
   })
 
+  // create
+  songs.post("/",  async (req, res) => {
+    try {
+      const song = await createSong(req.body);
+      res.json(song);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
+  });
 
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -21,6 +21,20 @@ const getAllSongs = async () => {
     }
   };
 
-module.exports = { getAllSongs, getSong };
+// CREATE
+const createSong = async (song) => {
+  try {
+    const newSong = await db.one(
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+
+module.exports = { getAllSongs, getSong, createSong };
 
 
