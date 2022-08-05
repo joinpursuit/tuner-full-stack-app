eb617c691d7165e25e21c289e02060a54c1f78c9
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-04 22:23:38 -0400

    index

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,4 +3,5 @@
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Dont Stop Till You Get Enough', 'Michael Jackson', 'Off the Wall', '1979', true),
 ('Good Morning Gorgeous', 'Mary J. Blige', 'Good Morning Gorgeous', '2022', true),
+('Right On', 'Lil Baby', 'Mix Tape', '2022', false),
 ('Mixed Signals', 'Daniel John', 'Bad Connections', '2022', true);
---
+++ back-end/queries/songs.js
@@ -0,0 +1,19 @@
+const db = require("../db/dbConfig.js");
+
+
+
+
+const getAllSongs = async () => {
+    try {
+      const allSongs = await db.any("SELECT * FROM songs");
+      return allSongs;
+    } catch (error) {
+      return error;
+    }
+  };
+
+
+
+module.exports = { getAllSongs };
+
+
