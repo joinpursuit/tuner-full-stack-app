0d1f49c7b9bb13d48c3d1b5511a51f5f39227443
Dannell <dannellmorales@pursuit.org>
2022-08-10 11:13:46 -0400

    Aug 10th commit

---
+++ back-end/controllers/reviewsController.js
@@ -0,0 +1,8 @@
+const express = require ("express") ;
+
+const songReviews = express.Router();
+
+reviews.get("/", async (req, res)=> {} )
+
+
+module.export = reviews;
---
+++ back-end/queries/reviews.js
@@ -0,0 +1,9 @@
+const db = required ("../db/dbconfir.js")
+
+const getAllSongReviews = async () => {
+    
+    
+    try{
+        const all
+    }catch (err)
+}
