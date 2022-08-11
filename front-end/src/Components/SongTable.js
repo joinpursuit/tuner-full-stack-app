13051126240611dec8a5651b89cb32b5032ce373
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-08-10 21:54:41 -0400

    Good alert messages from snackbar

--- front-end/src/Components/SongTable.js
+++ front-end/src/Components/SongTable.js
@@ -12,6 +12,7 @@ import axios from "axios";
           - Will need different methods for any others I may choose
             to implement
     - Edit button brings you to do songs individual edit page
+    - Set Snackbar to show after toggling favorite status
 */
 
 // MUI Imports
@@ -25,55 +26,99 @@ import Paper from "@mui/material/Paper";
 import { Button } from "@material-ui/core";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { handleBreakpoints } from "@mui/system";
 
 const API_KEY = process.env.REACT_APP_API_URL;
 
+//Code taken from example on MUI documentation.
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 export const SongTable = ({ songs }) => {
+  const [open, setOpen] = React.useState();
   const tableHeaders = ["Title", "Artist", "Album", "Length", "Favorite"];
+  const [alertHeaders, setAlertHeaders] = React.useState({
+    severity: "",
+    body: "",
+  });
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleClick = () => {};
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minwidth: 650 }} aria-label="Song Playlist">
-        <TableHead>
-          <TableRow>
-            {tableHeaders.map((header) => {
-              return <TableCell align="center">{header}</TableCell>;
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minwidth: 650 }} aria-label="Song Playlist">
+          <TableHead>
+            <TableRow>
+              {tableHeaders.map((header) => {
+                return <TableCell align="center">{header}</TableCell>;
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {songs.map((song) => {
+              return (
+                <TableRow key={song.id}>
+                  <Song song={song} />
+                  <TableCell>
+                    <Button
+                      onClick={() => {
+                        const fav = song.is_favorite ? "false" : "true";
+                        axios.put(`${API_KEY}/songs/${song.id}`, {
+                          name: song.name,
+                          artist: song.artist,
+                          album: song.album,
+                          time: song.time,
+                          is_favorite: fav,
+                        });
+                        //This part took shamefully way too long to get
+                        fav === "true"
+                          ? setAlertHeaders({
+                              severity: "success",
+                              body: "Song added to favorites",
+                            })
+                          : setAlertHeaders({
+                              severity: "info",
+                              body: "Song removed from favorites",
+                            });
+                        setOpen(true);
+                      }}
+                    >
+                      {song.is_favorite ? (
+                        <FavoriteIcon />
+                      ) : (
+                        <FavoriteBorderIcon />
+                      )}
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button href={`/songs/${song.id}/edit`}>Edit</Button>
+                  </TableCell>
+                </TableRow>
+              );
             })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {songs.map((song) => {
-            return (
-              <TableRow key={song.id}>
-                <Song song={song} />
-                <TableCell>
-                  <Button
-                    onClick={() => {
-                      const fav = song.is_favorite ? "false" : "true";
-                      axios.put(`${API_KEY}/songs/${song.id}`, {
-                        name: song.name,
-                        artist: song.artist,
-                        album: song.album,
-                        time: song.time,
-                        is_favorite: fav,
-                      });
-                    }}
-                  >
-                    {song.is_favorite ? (
-                      <FavoriteIcon />
-                    ) : (
-                      <FavoriteBorderIcon />
-                    )}
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button href={`/songs/${song.id}/edit`}>Edit</Button>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={alertHeaders.severity}
+          sx={{ width: "100%" }}
+        >
+          {alertHeaders.body}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 };
