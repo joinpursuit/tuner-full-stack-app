670261ff7479391a5480b6270d02ebf95e3ed126
Edwin Codrington <edwincodrington@gmail.com>
2022-08-11 21:54:54 -0400

    skeleton

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,26 @@
+import { Routes, Route } from "react-router";
+import NavBar from "./Components/NavBar";
+import Songs from "./Pages/Songs";
+import EditSong from "./Pages/EditSong";
+import NewSong from "./Pages/NewSong";
+import ShowSong from "./Pages/ShowSong";
+import Home from "./Components/Home";
+
 function App() {
-  return (
-    <div className="">
-      <h1>Tuner App</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<NavBar />
+			</header>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/songs" element={<Songs />} />
+				<Route path="/songs/:id" element={<ShowSong />} />
+				<Route path="/songs/new" element={<NewSong />} />
+				<Route path="/songs/:id/edit" element={<EditSong />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ front-end/src/Pages/NewSong.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewSong() {
+  return (
+    <div>NewSong</div>
+  )
+}
