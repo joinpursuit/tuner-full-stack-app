3b45c3687dce5518b77829a5cafffa6c6981bc63
Victor E Lopez <velbx51594@macbook-air.mynetworksettings.com>
2022-08-15 21:44:49 -0400

    front end functionality but not passing tests

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,5 +2,4 @@
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Fame', 'David Bowie', 'Young Americans', '4:12' , true);
--- ('Apartment Therapy', 'https://www.apartmenttherapy.com', 'inspirational', true),
--- ('DMV', 'https://dmv.ny.gov', 'adulting', true);
+
